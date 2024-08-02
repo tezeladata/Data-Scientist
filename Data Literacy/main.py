23b7b38067db59279fb80fb03a8760bd17645201
tezeladata <David.tezela@gmail.com>
2024-08-02 17:14:13 +0400

    data literacy

---
+++ Data Literacy/main.py
@@ -0,0 +1,69 @@
+#  Sometimes called Data Scientists, sometimes called Data Analysts, they are the people answering questions with data
+
+# This Career Path is divided into 2 parts. The first section covers all the basics every Data Scientist needs. We call this the Foundations of Data Science. The second section is the specialized KSAs you need to go from being a data generalist to an Analytics-based Data Scientist.
+
+
+
+# A database is a set of data stored in a computer. This data is usually structured into tables. Tables can grow large and have a multitude of columns and records.
+# SQL (pronounced “S-Q-L” or “sequel”) allows you to write queries which define the subset of data you are seeking. Unlike Excel and Sheets, your computer and SQL will handle how to get the data; you can focus on what data you would like. You can save these queries, refine them, share them, and run them on different databases.
+
+
+# Churn rate is the percent of subscribers to a monthly service who have canceled. For example, in January, let’s say Codecademy had 1,000 learners. In February, 200 learners sign up, and 250 cancel.
+# cancellations / total subscribers = 250 / (1000 + 200) = 20/8%
+
+
+# Python is a programming language, and pandas is a special set of commands in Python that lets us analyze spreadsheet data. Pandas can do a lot of the things that SQL can do, but it’s also backed by the power of Python, so we can easily transition from analyzing our data with pandas to visualizing it using other Python tools.
+
+
+# It’s no secret that data is an incredibly powerful tool. With all that’s at stake, it’s also not a surprise that understanding a data-driven conclusion can feel overwhelming sometimes – both as an audience member and as an analyst. No matter which side we find ourselves on, data literacy is about how well we read, interpret, and communicate with data.
+
+
+# Garbage in, garbage out is a data-world phrase that means “our data-driven conclusions are only as strong, robust, and well-supported as the data behind them.”
+
+
+# Part of practicing good data literacy means asking…
+
+# Who participated in the data?
+# Who is left out?
+# Who made the data?
+
+
+# Statistics helps us test the likelihood of an event happening by random chance versus systematically.
+
+
+# Data visualization is one of the most visible and obvious places we interact with data. It helps us to explore and understand data-driven arguments and is a powerful tool for communication.
+
+
+# Part of an analyst’s job is to provide context and clarifications to make sure that audiences are not only reading the correct numbers, but understanding what they mean.
+
+
+# Data can come from a lot of places, including research, governments, technology, observation, or directly from individuals — the list is endless!
+
+
+# Data can mean a lot of things, but within data science, it typically means a collection of organized observations.
+
+
+# The most common shape for data is a spreadsheet or table. The things we are measuring (variables) are in the columns, and the individual instances (observations) are in the rows. We can read each column “down” the table (viewing multiple observations), and each row “across” the table (viewing multiple variables).
+
+
+# In our tree census, we are collecting data about two types of variables: one that we measure (height) and one that we categorize (species).
+
+
+# In our tree census, we are collecting data about two types of variables: one that we measure (height) and one that we categorize (species).
+
+
+# Different problems need to be handled differently, so let’s categorize them:
+# Typos like Tuuullip for Tulip
+# Missing data like the Pin Oak (tree ID 11222) that doesn’t have a height
+# Inconsistent coding like the Pin Oak (tree 18564)’s Prettiness value is ‘three’ rather than ‘3’ and the Single value for all of our trees is ‘no’ rather than ‘0’.
+
+
+# Accuracy is a measure of how well records reflect reality.
+
+
+# Validity is a special kind of quality measure because it’s not just about the dataset, it’s about the relationship between the dataset and its purpose. A dataset can be valid for one question and invalid for another.
+
+
+# Best practice is to create a sample that represents the entire population.
+
+# The population is all of the trees in the North Atlantic region. The sample is the trees that we have data about (it will almost never be all of them).
